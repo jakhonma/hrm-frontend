41a505fc0a568b7e6b89de0ff45f2c8d85143ef1
Abdixoliqov <abdixoliqovhamidullo@gmail.com>
2025-08-21 11:20:17 +0500

    added icon

--- src/components/Helper.jsx
+++ src/components/Helper.jsx
@@ -1,20 +1,28 @@
 import { TbHelpHexagon } from "react-icons/tb";
 import { IoDocumentTextOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
+import { PiHeadphonesThin } from "react-icons/pi";
+import { BiBot } from "react-icons/bi";
+import { PiChatCircleDotsThin } from "react-icons/pi";
+import { PiChatDotsThin } from "react-icons/pi";
+import { PiHeadsetThin } from "react-icons/pi";
+
+import { PiReadCvLogoThin } from "react-icons/pi";
+import { PiFileTextThin } from "react-icons/pi";
 
 
 function Helper() {
     return ( 
-        <div className="py-4 text-gray-500 space-y-1 w-full border-t flex items-center justify-start gap-5 ml-5">
+        <div className="py-4 text-gray-500 space-y-1 w-full border-t flex items-center justify-start gap-6 ml-5 mt-6">
           <Link to="/" className="rounded hover:bg-grare-200">
-            <div className="w-full py-1 flex gap-2 items-center">
-                <TbHelpHexagon size={23} className="" />
+            <div className="w-full py-1 flex gap-1 items-center">
+                <PiHeadsetThin size={21} className="" />
                 <div className="text-[15px]">Texnik yordam</div>
             </div>
           </Link>
           <Link to="/" className="rounded hover:bg-grare-200">
-            <div className="w-full py-1 flex gap-2 items-center">
-                <IoDocumentTextOutline size={23} className="" />
+            <div className="w-full py-1 flex gap-[2px] items-center">
+                <PiFileTextThin size={22} className="" />
                 <div className="text-[15px]">Hujjatlar</div>
             </div>
           </Link>
--- src/pages/organization/Organization.jsx
+++ src/pages/organization/Organization.jsx
@@ -58,7 +58,7 @@ function Organization() {
     },
   ];
     return ( 
-        <div className="p-6 ">
+        <div className="ml-6">
       {/* Header */}
       <div className="flex justify-between items-center mb-6">
         <h1 className="text-2xl font-semibold text-gray-700">
